32a90a26b1a88f39f5ae7553acfbea87657d71c8
pilar0000 <pilarescolarsuarez@hotmail.com>
2025-11-24 16:23:41 +0100

    Codigo de la case Herramienta y sus subclases

--- src/juego/Arco.java
+++ src/juego/Arco.java
@@ -1,5 +1,21 @@
 package juego;
 
-public class Arco {
+// el arco tiene un bonus de ataque medio y mejora con mas frecuencia
 
+public class Arco extends Herramienta {
+	
+	// constructor del arco, crea un arco con bonus 7
+	public Arco() {
+		super("Arco", 7);
+	}
+	
+	// la probabilidad de que mejore es del 15%
+	public boolean comprobarModificacion() {
+		return Math.random() < 0.15;
+	}
+	
+	// suma un punto de ataque en el bonus
+	public void modifica() {
+		bonusAtaque += 1;
+	}
 }
--- src/juego/Espada.java
+++ src/juego/Espada.java
@@ -1,5 +1,21 @@
 package juego;
 
-public class Espada {
+// la espada tiene un bonus inicial alto y mejora con poca probabilidad
 
+public class Espada extends Herramienta {
+	
+	// constructor creando una espada con bonus 10
+	public Espada() {
+		super("Espada", 10);
+	}
+	
+	// la probabilidad de que mejore es del 10%
+	public boolean comprobarModificacion() {
+		return Math.random() < 0.10;
+	}
+	
+	// la mejora suma 2 puntos de ataque en el bonus
+	public void modifica() {
+		bonusAtaque += 2;
+	}
 }
