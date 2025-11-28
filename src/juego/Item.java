2339e92ecfc48985d03b4faa6ddb2e0ba2e31527
pilar0000 <pilarescolarsuarez@hotmail.com>
2025-11-28 10:33:21 +0100

    he cambiado cosas que estaban mal y he añadido Roll

--- src/juego/Item.java
+++ src/juego/Item.java
@@ -3,19 +3,37 @@ package juego;
 // objeto que puede aparecer en el suelo
 
 public class Item {
-	
-	private TipoItem tipo;
-	
-	public Item(TipoItem tipo) {
-		this.tipo = tipo;
-	}
-	
-	public TipoItem getTipo() {
-		return tipo;
-	}
-	
-	@Override
-	public String toString() {
-		return "Item: " + tipo;
-	}
+
+    private TipoItem tipo;
+    
+    // constructor
+    public Item(TipoItem tipo) {
+        this.tipo = tipo;
+    }
+    
+    //  ver si es pocion de vida o de mana
+    public TipoItem getTipo() { 
+    	return tipo; 
+    }
+    
+    // aplicar los efectos al jugador
+    public void aplicar(Roll r) {
+
+        switch (tipo) {
+
+            case POCION_VIDA:
+                int nuevaVida = Math.min(r.getVidaMax(), r.getVida() + 20);
+         
+                while (r.getVida() < nuevaVida) {
+                    r.recibirAtaque(-1);
+                }
+                break;
+
+            case POCION_MANA:
+                while (r.getMana() < r.getManaMax()) {
+                    r.mana += 1;
+                }
+                break;
+        }
+    }
 }
